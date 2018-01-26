901822c2ca765462d9d90be56ffbdf50ba06c205
Alex1OPS <ale51098223@yandex.ru>
2018-01-26 15:39:48 +0600

    Завершение writer для формирование документов в csv

--- writers/csvwriter.py
+++ writers/csvwriter.py
@@ -7,11 +7,21 @@ def generate_update_log_context(doc_entity):
 
 
 def generate_changelist_context(doc_entity):
-    return [], []
+    list_patch = [[iproject, jpatch] for iproject in doc_entity.projects for jpatch in iproject.patches]
+    return ["project", "version", "comment", "db_changed", "ui_changed"], \
+           [{"project": item[0].project_name,
+             "version": item[1].version_num,
+             "comment": " ".join(item[1].comment.split()),
+             "db_changed": ",".join(map(str, item[1].db_changed)),
+             "ui_changed": ",".join(map(str, item[1].ui_changed))} for item in list_patch]
 
 
 def generate_builds_changelist_context(doc_entity):
-    return [], []
+    dsub_builds = [x for x in doc_entity.projects if x.patches and len(x.patches) != 0]
+    return ["project", "num", "comment"], \
+           [{"project": xproj.project_name,
+             "num": str(ypatch.version_num),
+             "comment": " ".join(ypatch.comment.split())} for xproj in dsub_builds for ypatch in xproj.patches]
 
 
 def render_save(columns, context, path_to):
